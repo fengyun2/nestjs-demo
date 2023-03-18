3665846d32cb12f4c23045198ae043c3b1dd95a6
ly2011 <1518550424@qq.com>
2023-03-18 22:43:12 +0800

    feat: Get和Post请求demo

--- src/girl/girl.controller.ts
+++ src/girl/girl.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Request, Query, Body } from '@nestjs/common';
 import { GirlService } from './girl.service';
 
 @Controller('girl')
@@ -9,7 +9,14 @@ export class GirlController {
     return this.girlService.getGirls();
   }
   @Post('/add')
-  addGirl(): any {
+  addGirl(@Body() body): any {
+    console.log(body);
     return this.girlService.addGirl();
+  }
+  @Get('/getGirlById')
+  getGirlById(@Query() query): any {
+    // 因为通过 Get 放松传递过来的是字符串，所以我们需要用 parseInt 转化为数字
+    const id: number = parseInt(query.id, 10);
+    return this.girlService.getGirlById(id);
   }
 }
--- src/girl/girl.service.ts
+++ src/girl/girl.service.ts
@@ -16,4 +16,19 @@ export class GirlService {
       msg: '女孩添加成功',
     };
   }
+  getGirlById(id: number): any {
+    let reJson: any = {};
+    switch (id) {
+      case 1:
+        reJson = { id: 1, name: '翠花', age: 18 };
+        break;
+      case 2:
+        reJson = { id: 2, name: '小红', age: 20 };
+        break;
+      case 3:
+        reJson = { id: 3, name: '大丫', age: 23 };
+        break;
+    }
+    return reJson;
+  }
 }
