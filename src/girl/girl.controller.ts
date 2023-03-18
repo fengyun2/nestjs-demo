a0936790ae3bcc42edb297b41ce3aa69d8b4ca48
ly2011 <1518550424@qq.com>
2023-03-18 22:50:52 +0800

    feat: 动态路由的创建

--- src/girl/girl.controller.ts
+++ src/girl/girl.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Request, Query, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Request,
+  Query,
+  Body,
+  Param,
+  Headers,
+} from '@nestjs/common';
 import { GirlService } from './girl.service';
 
 @Controller('girl')
@@ -17,6 +26,13 @@ export class GirlController {
   getGirlById(@Query() query): any {
     // 因为通过 Get 放松传递过来的是字符串，所以我们需要用 parseInt 转化为数字
     const id: number = parseInt(query.id, 10);
+    return this.girlService.getGirlById(id);
+  }
+  @Get('/findGirlById/:id/:name')
+  findGirlById(@Param() params, @Headers() header): any {
+    console.log(params.name);
+    console.log(header);
+    const id: number = parseInt(params.id);
     return this.girlService.getGirlById(id);
   }
 }
