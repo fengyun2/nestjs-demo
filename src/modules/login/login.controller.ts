04928e07c36761c106efae7f034d8d7b199aa446
ly2011 <1518550424@qq.com>
2023-03-25 23:49:44 +0800

    feat: 实现JWT功能

--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -3,12 +3,16 @@ import { ApiTags } from '@nestjs/swagger';
 import { ReqLoginDto } from './dto/req-login.dto';
 import { LoginService } from './login.service';
 import { LocalAuthGuard } from '../../common/guards/local-auth.guard';
+import { AuthService } from '../system/auth/auth.service';
 import type { Request } from 'express';
 
 @ApiTags('登录')
 @Controller()
 export class LoginController {
-  constructor(private readonly loginService: LoginService) {}
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly authService: AuthService,
+  ) {}
 
   // 用户登录
   @Post('/auth/login')
@@ -17,6 +21,7 @@ export class LoginController {
     @Body() reqLoginDto: ReqLoginDto,
     @Req() req: Request,
   ): Promise<any> {
-    return req.user;
+    // return req.user;
+    return this.authService.login(req.user);
   }
 }
--- src/modules/system/auth/auth.service.ts
+++ src/modules/system/auth/auth.service.ts
@@ -3,10 +3,14 @@
  */
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
@@ -15,5 +19,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any): Promise<any> {
+    // 注意：我们选择 sub 的属性名来保持我们的 userId 值与 JWT 标准一致。
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
